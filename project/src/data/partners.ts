a7444c277168d1f1529ea083314133b1dc6fe730
Laxmi Nivas Morishetty <laxminivasmorishetty143@gmail.com>
2025-11-16 18:58:37 +0530

    main

--- project/src/data/partners.ts
+++ project/src/data/partners.ts
@@ -1,18 +1,20 @@
 import { Partner } from '../types';
 
 export const partners: Partner[] = [
-  {
-    name: "Wisdom Roots",
-    description: "Educational platform fostering deep learning and knowledge sharing",
-    website: "https://wisdomroots.netlify.app/",
-    logo: "/images/wr.png"
-  },
+  
   {
     name: "Smart N Light Innovation",
     description: "Technology innovation hub for smart solutions and digital transformation",
     website: "https://smartnlightinnovation.netlify.app/",
     logo: "/images/snl.png"
   },
+  {
+    name: "Wisdom Roots",
+    description: "Educational platform fostering deep learning and knowledge sharing",
+    website: "https://wisdomroots.netlify.app/",
+    logo: "/images/wr.png"
+  },
+ 
   {
     name: "Padma Tutorials",
     description: "Comprehensive learning platform offering expert tutorials and skill development",
